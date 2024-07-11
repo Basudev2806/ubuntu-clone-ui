1a044eed113374ac95d94912d2f0dddd3ddd35f9
Basudev2806 <basudev.kar.kolkata@gmail.com>
2024-07-11 21:17:35 +0530

    Ubuntu Clone

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,28 +1,69 @@
-import React from 'react';
-import { FaFolder, FaFirefox, FaTerminal, FaMusic } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import firefoxIcon from '../../assets/icons/firefox.png';
+import folderIcon from '../../assets/icons/folder.png';
+import terminalIcon from '../../assets/icons/terminal.png';
+import settingsIcon from '../../assets/icons/settings.png';
+import wordIcon from '../../assets/icons/word.png';
+import excelIcon from '../../assets/icons/excel.png';
+import networkIcon from '../../assets/icons/network.png';
+import batteryIcon from '../../assets/icons/battery-status.png';
+import volumeIcon from '../../assets/icons/volume-up.png';
 import './HomeScreen.css'; // Add your CSS for styling
 
 const HomeScreen = () => {
+    
+    const [currentTime, setCurrentTime] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const now = new Date();
+          const dayName = getDayName(now.getDay());
+          const formattedTime = formatTime(now.getHours(), now.getMinutes());
+          setCurrentTime(`${dayName}, ${formattedTime}`);
+        }, 1000);
+    
+        return () => clearInterval(interval);
+      }, []);
+
+      const getDayName = (dayIndex) => {
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        return days[dayIndex];
+      };
+
+      const formatTime = (hours, minutes) => {
+        const period = hours >= 12 ? 'PM' : 'AM';
+        const twelveHourFormat = hours % 12 || 12;
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        return `${twelveHourFormat}:${formattedMinutes} ${period}`;
+      };
+
   return (
     <div className="home-screen">
-        <div className='home-screen-icons'>
-            <div className="icon-container">
-                <FaFolder className="icon" />
-                <span>Files</span>
-            </div>
-            <div className="icon-container">
-                <FaFirefox className="icon" />
-                <span>Browser</span>
-            </div>
-            <div className="icon-container">
-                <FaTerminal className="icon" />
-                <span>Terminal</span>
-            </div>
-            <div className="icon-container">
-                <FaMusic className="icon" />
-                <span>Music</span>
-            </div>
+        <div className='header'>
+            <div><span>Activities</span></div>
+            <div className='current-time'>{currentTime}</div>
+            <div className='right-header'><img src={networkIcon} alt="Excel" className="icon-header" /> <img src={volumeIcon} alt="Excel" className="icon-header" /> <img src={batteryIcon} alt="Excel" className="icon-header" /> </div>
         </div>
+      <div className='home-screen-icons'>
+        <div className="icon-container">
+          <img src={folderIcon} alt="Folder" className="icon" /> 
+        </div>
+        <div className="icon-container">
+          <img src={firefoxIcon} alt="Browser" className="icon" />
+        </div>
+        <div className="icon-container">
+          <img src={terminalIcon} alt="Terminal" className="icon" /> 
+        </div>
+        <div className="icon-container">
+          <img src={settingsIcon} alt="Settings" className="icon" /> 
+        </div>
+        <div className="icon-container">
+          <img src={wordIcon} alt="Word" className="icon" /> 
+        </div>
+        <div className="icon-container">
+          <img src={excelIcon} alt="Excel" className="icon" /> 
+        </div>                        
+      </div>
     </div>
   );
 };

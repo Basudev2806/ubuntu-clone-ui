bd2f347f0ae4da721bac7d75887d18a1658948ba
Basudev2806 <basudev.kar.kolkata@gmail.com>
2024-07-18 14:17:30 +0530

    Meta Tag Generator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeScreen from './components/HomeScreen/HomeScreen';
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <HomeScreen />
+        <Analytics />
       </header>
     </div>
   );
